6033c97a12ec9a24366ac0de9ed9849ce1205458
MLW <mwhit74@gmail.com>
2016-06-02 12:18:13 -0500

    Revert "Added XYPoints and XYZPoints classes; modifiying section_property to be a class"
    This reverts commit 0a44ac798a08b78b04ea4f8347a6a25f1fb5d749.

--- section_property.py
+++ section_property.py
@@ -1,99 +1,86 @@
-from Points import XYPoint
+"""Section Properties"""
 
-class SectionProperty:
-    def __init__(xy_coords):
-        self.points = convert_to_points(xy_coords)
+def area(xy_coords):
+	return -1*loop(xy_coords, area_eq)
+	
+def area_eq(cur_x, cur_y, next_x, next_y):
+	return (next_y - cur_y)*(next_x + cur_x)/2.0
 
-    def convert_to_points(xy_coords):
-        points = []
-        for xy in xy_coords:
-            points.append(XYPoint(xy[0], xy[1]))
-        return points
+	
+def ENA_x(xy_coords):
+	A = area(xy_coords)
+	return -1.0/A*loop(xy_coords, ENA_x_eq)
+	
+def ENA_x_eq(cur_x, cur_y, next_x, next_y):
+	return ((next_y - cur_y)/8.0)*((next_x + cur_x)**2.0 + (next_x - cur_x)**2.0/3.0)
+	
 
-    def area(xy_coords):
-            return -1*loop(xy_coords, area_eq)
-            
-    def area_eq(cur_x, cur_y, next_x, next_y):
-            return (next_y - cur_y)*(next_x + cur_x)/2.0
+def ENA_y(xy_coords):
+	A = area(xy_coords)
+	return 1.0/A*loop(xy_coords, ENA_y_eq)
+	
+def ENA_y_eq(cur_x, cur_y, next_x, next_y):
+	return ((next_x - cur_x)/8.0)*((next_y + cur_y)**2.0 + (next_y - cur_y)**2.0/3.0)
+	
+	
+def I_about_x(xy_coords):
+	return loop(xy_coords, I_about_x_eq)
+	
+def I_about_x_eq(cur_x, cur_y, next_x, next_y):
+	return ((next_x - cur_x)*(next_y + cur_y)/24.0)*((next_y + cur_y)**2.0 + (next_y - cur_y)**2.0)
+	
+	
+def I_about_y(xy_coords):
+	return -1*loop(xy_coords, I_about_y_eq)
+	
+def I_about_y_eq(cur_x, cur_y, next_x, next_y):
+	return ((next_y - cur_y)*(next_x + cur_x)/24.0)*((next_x + cur_x)**2.0 + (next_x - cur_x)**2.0)
+	
+	
+def I_about_x_centroid(xy_coords):
+	A = area(xy_coords)
+	y_bar = ENA_y(xy_coords)
+	I_x = I_about_x(xy_coords)
+	
+	return (I_x - A*y_bar**2)
+	
 
-            
-    def ENA_x(xy_coords):
-            A = area(xy_coords)
-            return -1.0/A*loop(xy_coords, ENA_x_eq)
-            
-    def ENA_x_eq(cur_x, cur_y, next_x, next_y):
-            return ((next_y - cur_y)/8.0)*((next_x + cur_x)**2.0 + (next_x - cur_x)**2.0/3.0)
-            
+def I_about_y_centroid(xy_coords):
+	A = area(xy_coords)
+	x_bar = ENA_x(xy_coords)
+	I_y = I_about_y(xy_coords)
+	
+	return (I_y - A*x_bar**2)
 
-    def ENA_y(xy_coords):
-            A = area(xy_coords)
-            return 1.0/A*loop(xy_coords, ENA_y_eq)
-            
-    def ENA_y_eq(cur_x, cur_y, next_x, next_y):
-            return ((next_x - cur_x)/8.0)*((next_y + cur_y)**2.0 + (next_y - cur_y)**2.0/3.0)
-            
-            
-    def I_about_x(xy_coords):
-            return loop(xy_coords, I_about_x_eq)
-            
-    def I_about_x_eq(cur_x, cur_y, next_x, next_y):
-            return ((next_x - cur_x)*(next_y + cur_y)/24.0)*((next_y + cur_y)**2.0 + (next_y - cur_y)**2.0)
-            
-            
-    def I_about_y(xy_coords):
-            return -1*loop(xy_coords, I_about_y_eq)
-            
-    def I_about_y_eq(cur_x, cur_y, next_x, next_y):
-            return ((next_y - cur_y)*(next_x + cur_x)/24.0)*((next_x + cur_x)**2.0 + (next_x - cur_x)**2.0)
-            
-            
-    def I_about_x_centroid(xy_coords):
-            A = area(xy_coords)
-            y_bar = ENA_y(xy_coords)
-            I_x = I_about_x(xy_coords)
-            
-            return (I_x - A*y_bar**2)
-            
+	
+def loop(xy_coords, func):
+	var = 0
+	
+	cur_x = xy_coords[0][0]
+	cur_y = xy_coords[0][1]
+	
+	for xy in xy_coords:
+		next_x = xy[0]
+		next_y = xy[1]
+		
+		var = var + func(cur_x, cur_y, next_x, next_y)
+			
+		cur_x = next_x
+		cur_y = next_y
+	
+	return var
 
-    def I_about_y_centroid(xy_coords):
-            A = area(xy_coords)
-            x_bar = ENA_x(xy_coords)
-            I_y = I_about_y(xy_coords)
-            
-            return (I_y - A*x_bar**2)
+def graham_scan(xy_coords):
+	x = xy_coords[0][0]
+	y = xy_coords[0][1]
 
-            
-    def loop(xy_coords, func):
-            var = 0
-            
-            cur_x = xy_coords[0][0]
-            cur_y = xy_coords[0][1]
-            
-            for xy in xy_coords:
-                    next_x = xy[0]
-                    next_y = xy[1]
-                    
-                    var = var + func(cur_x, cur_y, next_x, next_y)
-                            
-                    cur_x = next_x
-                    cur_y = next_y
-            
-            return var
-
-    def graham_scan(xy_coords):
-            x = xy_coords[0][0]
-            y = xy_coords[0][1]
-
-            for xy in xy_coords:
-                    if xy[1] < y:
-                            y = xy[1]
-                            x = xy[0]
-                    if xy[1] = y and xy[0] < x:
-                            y = xy[1]
-                            x = xy[0]
-
-    def ccw(p1, p2, p3):
-
+	for xy in xy_coords:
+		if xy[1] < y:
+			y = xy[1]
+			x = xy[0]
+		if xy[1] = y and xy[0] < x:
+			y = xy[1]
+			x = xy[0]
 	
 if __name__ == "__main__":
 	xy_coords = [[0.0,0.0],[2.0,0.0],[2.0,2.0],[0.0,2.0],[0.0,0.0]] #rectangle
