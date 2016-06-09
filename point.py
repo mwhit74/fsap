0d66dd627893456302af015846c491f1509606be
spottedzebra <mwhit74@gmail.com>
2016-06-08 21:21:41 -0500

    Update point.py

--- point.py
+++ point.py
@@ -2,17 +2,6 @@
 XY and XYZ point classes. 
 """
 
-__author__ = "MLW"
-__copyright__ = ""
-__credits__ = ["MLW"]
-
-__license__ = ""
-__version__ = "1.0"
-__maintainer__ = "MLW"
-__email__ = "mwhit74@gmail.com"
-__status__ = "Development"
-
-
 class XYPoint:
     """Class to represent a point in 2D space."""
 
@@ -32,7 +21,7 @@ class XYPoint:
         return (self.x,self.y)
 
 class XYZPoint:
-    """Class to represent a point in 3D space."
+    """Class to represent a point in 3D space."""
 
     def __init__(self,x,y,z):
         """Initialize the class arguments x, y, and z.
