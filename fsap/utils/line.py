83b2f7717b11d49c17f7492bc5dd12968e48a229
mwhit74 <mwhit74@gmail.com>
2017-11-09 07:11:23 -0600

    Added 3D line class. Formatted some code.
    Copied and edited the 2D line class. I considered inheriting the 2D line class
    but I would just be overwriting the current functions with new functions so I
    opted to just write a whole new function.

--- fsap/utils/line.py
+++ fsap/utils/line.py
@@ -4,17 +4,17 @@ import math
 Line class
 "
 
-class TwoDLine:
+class Line2D:
     "Class to represent a line in space."
 
     def __init__(self, pt1, pt2):
         "Initialize the class arguments pt1 and pt2.
 
         Args:
-            pt1 (TwoDPoint or ThreeDPoint): first point
-            pt2 (TwoDPoint or ThreeDPoint): second point
+            pt1 (Point2D): first point
+            pt2 (Point2D): second point
 
-        *See point.py for TwoDPoint and ThreeDPoint class definition. 
+        *See point.py for Point2D class definition. 
         """
 
         self.pt1 = pt1
@@ -30,7 +30,32 @@ class TwoDLine:
                 pow(self.pt1.y-self.pt2.y,2))
 
 
-    #convert coordinates in to polar
+class Line3D:
+    "Class to represent a line in space."
+
+    def __init__(self, pt1, pt2):
+        "Initialize the class arguments pt1 and pt2.
+
+        Args:
+            pt1 (Point3D): first point
+            pt2 (Point3D): second point
+
+        *See point.py for Point3D class definition. 
+        """
+
+        self.pt1 = pt1
+        self.pt2 = pt2
+
+    def __str__(self):
+        """String representation of a line."""
+        return "".format(self.pt1.x, self.pt1.y, self.pt1.z,
+                         self.pt2.x, self.pt2.y, self.pt2.z)
+
+    def length(self):
+        """Calculate the length of a line."""
+        return math.sqrt(pow(self.pt1.x - self.pt2.x,2) +
+                         pow(self.pt1.y - self.pt2.y,2) +
+                         pow(self.pt1.z - self.pt2.z,2))
 
 
 
