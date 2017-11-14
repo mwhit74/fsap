0e1032324ae06a8891a4a878e045903534bb9c13
mwhit74 <mwhit74@gmail.com>
2017-11-14 07:15:05 -0600

    Updated section property class to use Point2D class and fixed methods.
    Updated the section property class to use the Point2D class.
    
    It now calculates everything when the class is initiated. Then the user only
    has to ask for the property instead of calling the correct functions.
    
    Changed around how some of the function calls are working.

--- fsap/section_property/section_property.py
+++ fsap/section_property/section_property.py
@@ -1,77 +1,86 @@
-from Points import XYPoint
+from Points import Point2D
 
 class SectionProperty:
     def __init__(xy_coords):
         self.points = convert_to_points(xy_coords)
 
+        self.area = self.area()
+        self.ena_x = self.ENA_x()
+        self.ena_y = self.ENA_y()
+        self.ixx_x = self.I_about_x()
+        self.iyy_y = self.I_about_y()
+        self.ixx_c = self.I_about_x_centroid()
+        self.iyy_c = self.I_about_y_centroid()
+
     def convert_to_points(xy_coords):
         points = []
         for xy in xy_coords:
-            points.append(XYPoint(xy[0], xy[1]))
+            points.append(Point2D(xy[0], xy[1]))
         return points
 
-    def area(xy_coords):
-            return -1*loop(xy_coords, area_eq)
+    def area():
+            return -1*loop(self.points, self.area_eq)
             
     def area_eq(cur_x, cur_y, next_x, next_y):
             return (next_y - cur_y)*(next_x + cur_x)/2.0
 
             
-    def ENA_x(xy_coords):
-            A = area(xy_coords)
-            return -1.0/A*loop(xy_coords, ENA_x_eq)
+    def ENA_x():
+            A = self.area
+            return -1.0/A*loop(self.points, self.ENA_x_eq)
             
     def ENA_x_eq(cur_x, cur_y, next_x, next_y):
             return ((next_y - cur_y)/8.0)*((next_x + cur_x)**2.0 + (next_x - cur_x)**2.0/3.0)
             
 
-    def ENA_y(xy_coords):
-            A = area(xy_coords)
-            return 1.0/A*loop(xy_coords, ENA_y_eq)
+    def ENA_y():
+            A = self.area
+            return 1.0/A*loop(self.points, self.ENA_y_eq)
             
     def ENA_y_eq(cur_x, cur_y, next_x, next_y):
             return ((next_x - cur_x)/8.0)*((next_y + cur_y)**2.0 + (next_y - cur_y)**2.0/3.0)
             
             
-    def I_about_x(xy_coords):
-            return loop(xy_coords, I_about_x_eq)
+    def I_about_x():
+            return loop(self.points, self.I_about_x_eq)
             
     def I_about_x_eq(cur_x, cur_y, next_x, next_y):
             return ((next_x - cur_x)*(next_y + cur_y)/24.0)*((next_y + cur_y)**2.0 + (next_y - cur_y)**2.0)
             
             
-    def I_about_y(xy_coords):
-            return -1*loop(xy_coords, I_about_y_eq)
+    def I_about_y():
+            return -1*loop(self.points, self.I_about_y_eq)
             
     def I_about_y_eq(cur_x, cur_y, next_x, next_y):
             return ((next_y - cur_y)*(next_x + cur_x)/24.0)*((next_x + cur_x)**2.0 + (next_x - cur_x)**2.0)
             
             
-    def I_about_x_centroid(xy_coords):
-            A = area(xy_coords)
-            y_bar = ENA_y(xy_coords)
-            I_x = I_about_x(xy_coords)
+    def I_about_x_centroid():
+            A = self.area
+            y_bar = self.ENA_y
+            I_x = self.I_about_x()
             
             return (I_x - A*y_bar**2)
             
 
     def I_about_y_centroid(xy_coords):
-            A = area(xy_coords)
-            x_bar = ENA_x(xy_coords)
-            I_y = I_about_y(xy_coords)
+            A = self.area
+            x_bar = self.ENA_x
+            I_y = self.I_about_y
             
             return (I_y - A*x_bar**2)
 
             
-    def loop(xy_coords, func):
-            var = 0
+    def loop(self.points, func):
+            var = 0.0
+           
+            cur_pt = self.points[0]
+            cur_x = cur_pt.x
+            cur_y = cur_pt.y
             
-            cur_x = xy_coords[0][0]
-            cur_y = xy_coords[0][1]
-            
-            for xy in xy_coords:
-                    next_x = xy[0]
-                    next_y = xy[1]
+            for pt in self.points:
+                    next_x = pt.x
+                    next_y = pt.y
                     
                     var = var + func(cur_x, cur_y, next_x, next_y)
                             
@@ -80,19 +89,19 @@ class SectionProperty:
             
             return var
 
-    def graham_scan(xy_coords):
-            x = xy_coords[0][0]
-            y = xy_coords[0][1]
+    #def graham_scan(xy_coords):
+    #        x = xy_coords[0][0]
+    #        y = xy_coords[0][1]
 
-            for xy in xy_coords:
-                    if xy[1] < y:
-                            y = xy[1]
-                            x = xy[0]
-                    if xy[1] = y and xy[0] < x:
-                            y = xy[1]
-                            x = xy[0]
+    #        for xy in xy_coords:
+    #                if xy[1] < y:
+    #                        y = xy[1]
+    #                        x = xy[0]
+    #                if xy[1] = y and xy[0] < x:
+    #                        y = xy[1]
+    #                        x = xy[0]
 
-    def ccw(p1, p2, p3):
+    #def ccw(p1, p2, p3):
 
 	
 if __name__ == "__main__":
