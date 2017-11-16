3a292b640c986ebb0cb112447beae37014c729d1
mwhit74 <mwhit74@gmail.com>
2017-11-15 19:51:19 -0600

    Started testing section property class

---
+++ fsap/test/section_property_test.py
@@ -0,0 +1,8 @@
+import unittest
+from fsap.section_property import section_property as sp
+
+class SectionPropertyTest(unittest.TestCase):
+    def setUp(self):
+        points = [(0,0),(0,1),(1,1),(1,0)]
+
+    def 
