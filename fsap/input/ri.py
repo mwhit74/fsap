28c02d6a57275305bb8429506fb330fa6076a30f
mwhit74 <mwhit74@gmail.com>
2018-02-13 06:26:23 -0600

    Writing functions to read input file.
    Noticed that these functions will need to be updated to read in different
    structure types (truss, 2D frame, 3D frame) because the section properties
    information required changes.
    
    Also for a 3D frame the joint location function will have to change to read in
    an additional coordinate (z).
    
    For now I am going to continue to write this as simple as possible. The question
    because do I have a different input file reader for each different type of input
    file or do I have on input file reader that handles everything?

--- fsap/input/ri.py
+++ fsap/input/ri.py
@@ -3,7 +3,6 @@ import numpy
 def read_input(path):
     
     ifile = open(path, 'r')
-    data = ifile.read()
     
     read_joint(data)
     read_support(data)
@@ -12,8 +11,53 @@ def read_input(path):
     read_element(data)
     read_load(data)
 
+
 def read_joint(data):
     jt = []
-    njt = data.readline()
-    for i in range(njt)
-        
+    njt = int(data.readline())
+    for i in range(njt):
+        text = data.readline()
+        x_coord = float(text.split(",")[0])
+        y_coord = float(text.split(",")[1])
+        jt.append((x_coord,y_coord))
+
+    return jt
+
+
+def read_support(data):
+    sup = []
+    ns = int(data.readline())
+    for i in range(ns):
+        text = data.readline()
+        jt = int(text.split(",")[0])
+        x_dof = int(text.split(",")[1])
+        y_dof = int(text.split(",")[2])
+        sup.append((jt, x_dof, y_dof))
+
+    return sup
+
+
+def read_matlprop(data):
+    matl = []
+    nm = int(data.readline())
+    for i in range(nm):
+        text = data.readline()
+        E = float(text)
+        matl.append(E)
+
+    return matl
+
+
+def read_sectprop(data):
+    sp = []
+    nsp = int(data.readline())
+    for i in range(nsp):
+        text = data.readline()
+        A = float(text)
+        sp.append(A)
+
+
+def read_element(data):
+
+
+def read_load(data):
