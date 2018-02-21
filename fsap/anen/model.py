0929e0cc31caffb1fc273371ded8532c46afe422
mwhit74 <mwhit74@gmail.com>
2018-02-20 22:12:19 -0600

    Started working on function to assemble structure stiffness matrix.
    Spent a lot time this evening playing with the object oriented parts of fsap.
    
    I would like to stay focused on the example I am working from in Kassimali even
    though it is a more procedural approach. First I need to understand in detail
    how it works then I can build it out.
    
    Saved the oop work in a seperate branch. I think I was on the right track just
    not where I wanted to be.
    
    Bed time...

--- fsap/anen/model.py
+++ fsap/anen/model.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def num_dof(sup, num_scj, num_jt):
     """Calculate the number of degrees of freedom of the structure.
@@ -27,10 +28,10 @@ def num_dof(sup, num_scj, num_jt):
 def scv(sup, num_scj, num_jt, num_dof):
     """Generate structure coordinate vector."""
 
-    str_cv = []
+    str_cv = np.empty(num_jt*num_scj)
     j = 0
     k = num_dof
-    for i in range(0, num_jt):
+    for i in range(num_jt):
         if i in sup[0]:
             for x in range(num_scj):
                 y = (i - 1)*num_scj + x
@@ -45,3 +46,9 @@ def scv(sup, num_scj, num_jt, num_dof):
                 y = (i - 1)*num_scj + x
                 str_cv[y] = j
                 j += 1
+
+def assemble_stiffness(num_dof, num_scj):
+    s = np.empty(num_dof, num_dof)
+    gk = np.zeros(2*num_scj, 2*num_scj)
+    for im in mprp[0]:
+        jb = mprp[im][
