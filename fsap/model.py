31c6b0a95383698f218fe1286c4f1ff944caa987
mwhit74 <mwhit74@gmail.com>
2018-02-23 07:13:05 -0600

    Added to the documentation.
    I am planning for the documentation of this example to over the top. I want
    it to be very detailed so that I have to think about exactly what the
    program is doing and why, and document it so I can return to my
    understanding later.
    
    This will serve when I embark I writing my version of it and how it all
    has to fit together.
    
    I will also probably be able to eventually borrow most of this documentation
    for my version of the program. Wa-hoo, documentation already done! I just
    get to write the code.

---
+++ fsap/model.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+def num_dof(sup, num_scj, num_jt):
+    """Calculate the number of degrees of freedom of the structure.
+
+    Loops through the user input support matrix counting the number of
+    restrainted DOFs, i.e. the 1's in the 1 and 2 columns, this is the
+    number of restrained coordinates. 
+
+    The total number of DOFs is comupted as the number of joints in the
+    structure which is the number of entries in the user input joint
+    matrix, or coordinate matrix, times the number of DOFs per joint,
+    or the number of structure coordinate joints.
+
+    The number of unrestrained, or free,  DOFs is then calculated as the
+    total number of DOFs minus the number of restrained DOFs. 
+    
+    Args:
+        sup (list): 2D array of joint number, x support condition, y
+                          support condition
+        num_scj (int): number of structure coordinates per joint; number of
+                       degrees of freedom per joint
+        num_jt (int): number of joints in the structure
+
+    Returns:
+        ndof (int): number of degrees of freedom of the structure
+
+    Notes:
+        num_reacs (int): number of support reactions of the structure
+        num_scj: truss=2, 2D frame=3, 3D frame=6
+    """
+    num_reacs = 0
+    for i in range(len(sup)):
+        for j in range(2, num_scj+1):
+            if sup(i,j) == 1:
+                num_reacs += 1
+    ndof = num_scj*num_jt - num_reacs
+    return ndof
+
+def scv(sup, num_scj, num_jt, num_dof):
+    """Generate structure coordinate vector.
+    
+    Args:
+        sup (list): 2D array of joint number, x support condition, y support
+                    condition
+        num_scj (int): number of structure coordinates per joint; number of
+                       degrees of freedom per joint
+        num_jt (int): number of joints in the structure
+        num_dof (int): number of degrees of freedom of the structure
+
+    Returns:
+        str_cv (numpy array): 1D array (vector) of the numbered DOFs for
+                              the entire structure to the joints
+    """
+
+    str_cv = np.empty(num_jt*num_scj)
+    j = 0
+    k = num_dof
+    for i in range(num_jt):
+        if i in sup[0]:
+            for x in range(num_scj):
+                y = (i - 1)*num_scj + x
+                if sup[i][x] == 1:
+                    str_cv[y] = k
+                    k += 1
+                else:
+                    str_cv[y] = j
+                    j += 1
+        else:
+            for x in range(num_scj):
+                y = (i - 1)*num_scj + x
+                str_cv[y] = j
+                j += 1
+
+     return str_cv
+
+def assemble_stiffness(num_dof, num_scj):
+    """Assemble structure stiffness matrix."""
+
+    s = np.empty(num_dof, num_dof)
+    gk = np.zeros(2*num_scj, 2*num_scj)
+    for im in range(len(elem)):
+        jb = elem[im][0]
+        je = elem[im][1]
+        sp_id = elem[im][2]
+        a = sect[sp_id]
+        matl_id = elem[im][3]
+        e = matl[matl_id]
+        xb = jt[im][0]
+        yb = jt[im][1]
+        xe = jt[im][0]
+        ye = jt[im][1]
+        bl = math.sqrt(math.exp(xe-xb,2) + math.exp(ye-yb,2))
+        c = (xe-xb)/bl
+        s = (ye-yb/bl
+
+        mstiffg(e,a,bl,c,s,gk)
+        stores()
+
+def mstiffg(e,a,bl,c,s,gk):
+    """Member global stiffness matrix."""
+
+    eal = e*a/bl
+    c2 = math.pow(c,2)
+    s2 = math.pow(s,2)
+    sc = s*c
+
+    z1 = eal*c2
+    z2 = eal*s2
+    z3 = eal*sc
+
+    gk[0][0] = z1
+    gk[1][0] = z3
+    gk[2][0] = -z1
+    gk[3][0] = -z3
+    
+    gk[0][1] = z3
+    gk[1][1] = z2
+    gk[2][1] = -z3
+    gk[3][1] = -z2
+
+    gk[0][2] = -z1
+    gk[1][2] = -z3
+    gk[2][2] = z1
+    gk[3][2] = z3
+
+    gk[0][3] = -z3
+    gk[1][3] = -z2
+    gk[2][3] = z3
+    gk[3][3] = z2
+
+def stores(jb, je, num_scj, num_dof, scv, gk, s):
+    for i in range(2*num_scj):
+        if i <= num_scj:
+            y = (jb - 1)*num_scj + 1
+        else:
+            y = (je - 1)*num_scj + (i - num_scj)
+
